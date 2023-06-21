67b1ce46ea84bb01303130c26bdda06fd7d6b52d
I-bug <omaugulu@gmail.com>
2023-06-21 22:54:24 +0100

    add all js files

---
+++ app.js
@@ -0,0 +1,383 @@
+function calculateAreaOfRectangle(length, width) {
+    const area = length * width;
+    return area;
+}
+const moreOp = calculateAreaOfRectangle(10.5, 2.5) + 2;
+
+console.log(moreOp);
+
+
+    // console.log("Area of the Rectangle is: " + area)
+// }
+// console.log(calculateAreaOfRectangle(10.5, 2.5));
+
+
+
+
+
+
+
+// function converter(dollar) {
+//     // convert to naira and return the equivalent naira value
+//     // conversion rate: 410 naira to 1 dollar
+//     const naira = dollar * 410
+//     return naira;
+// }
+
+// const nairaValue = converter(100)
+
+// console.log(nairaValue)
+
+
+
+
+// TASK 9 ON FUNCTION
+// function calculateAreaOfRectangle(length, width) {
+//     const area = length * width;
+//     console.log("Area of the Rectangle is: " + area)
+// }
+// calculateAreaOfRectangle(10.5, 2.5);
+
+
+
+
+
+
+
+
+// RETURN FUNCTION LESSON
+// function addNumbers (firstNumber, secondNumber) {
+//     const sum = firstNumber + secondNumber;
+//     return sum;
+// }
+// const moreOp = addNumbers(7.9, 4.9) + 20;
+// console.log(moreOp);
+
+//console.log(addNumbers(7.9, 4.9));
+
+
+
+
+
+// FUNCTION LESSON
+// function addNumbers (firstNumber, secondNumber) {
+//     const sum = firstNumber + secondNumber;
+// }
+
+//console.log (addNumbers(7.9, 4.9));
+
+
+
+
+
+// function greeting(name) {
+//     console.log('Good morning,  ' +  name);
+// }
+
+// greeting('Laurel');
+
+
+
+
+// function MyFunction(){ //this is the functio declaration
+//     console.log('My first function!');
+// }
+
+// MyFunction(); // this is the fuction call
+
+
+
+
+
+
+
+
+
+// FOR-LOOP TASK
+// for (let i =1; i <= 20; i++) {
+//     if(i % 2 !== 0)
+//     console.log(i)
+// }
+
+
+
+
+// let number = 1
+// while (number <= 20){
+//     if(number % 2!== 0)
+//     console.log(number)
+// }
+
+
+
+
+
+// FOR-LOOP LESSON
+// for(let star = 1; star <=100; star = star+1) {
+//     console.log(star + ' star')
+// }
+
+
+
+
+
+// LESSON FOR WHILE LOOP
+//let star = 1 // starting point
+// while (star <=10000){
+//     if(star===1){
+//         console.log(star + ' star')
+//     } else{
+//         console.log(star + ' stars')
+//     }
+//         star = star + 1
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SWITCH STATEMENT FOR TASK 7
+// const triangleTypes= ('side1, side2, side3')
+// let side1= 17
+// let side2=19
+// let side3= 12
+
+// switch(triangleTypes){
+//     case(side1===side2 && side2===side3):
+//         console.log('This is an Equilateral triangle')
+//         break
+//     case(side1===side2 || side2===side3 || side1===side3):
+//         console.log('This is an Isoceles triangle')
+//         break
+//     default:
+//         console.log('This is a Scalene triangle')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TASK 7 IS BELOW USINH IF...ELSE..IF
+// const side1= 17
+// const side2= 17
+// const side3= 10
+
+// if(side1===side2 && side2===side3){
+//     console.log('Equilateral trainagle')
+// }else if(side1===2 || side2===side3 || side1===side3){
+//     console.log('Isoceles triangle')
+// }else {
+//     console.log('Scalene triangle')
+// }
+
+
+
+
+
+// SWITCH STATEMENT 
+// const day = 'Thursday'
+// switch(day){
+//     case 'Friday':
+//         console.log('TGIF')
+//         break
+//     case 'Saturday':
+//         console.log('Yeh! The weekend is here')
+//         break
+//     case 'Sunday':
+//         console.log('Happy Sunday!')
+//         break
+//     default:
+//         console.log('Go to work!')
+
+// }
+
+
+
+
+
+// const age =78 
+
+// // this is the test for IF...ELSE..IF statements
+
+// if(age >= 18 && age <= 65) {
+
+// console.log('You are eligible to vote')
+// } else if(age>65) {
+//    console.log('You are too old to vote')
+// } else {
+// console.log('You are too young to vote')
+// }
+                    // OR
+// const day = 'Thursday'
+
+// if(day==='Friday') {
+//     console.log('TGIF')
+// } else if(day==='Saturday') {
+//     console.log('Yeh! The weekend is here')
+// } else if(day==='Sunday') {
+//     console.log('Happy Sunday!')
+// } else {
+//     console.log('Go to work!')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TASK 6 ASSIGNMENT BELOW
+// const number = 65
+
+// // check if the given number is divisible by 2 i.e. if the number is an even or odd number 
+
+// if(number % 2 === 0) {
+
+//     console.log('Number is even.')
+
+// } else {
+   
+//     console.log('Number is odd.')
+
+// }
+// console.log(number%2===0)
+
+
+
+
+
+//My attempts for the IF/IF ELSE Statements task above are below;
+// console.log(number %2===0)
+// console.log(compare)
+// console.log('Number is even') + '.';
+// } else {
+//    console.log('Number is odd') + '.'
+// }
+
+
+
+
+
+
+
+
+// const age = 15 // this is the test for IF statement and ELSE statement
+
+// if (age >=18) {
+
+// console.log('You are eligible to vote')
+// } else {
+//    console.log('You are too young to vote')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const compare = 4==4
+
+/* 
+AND --> && all must be true
+OR ---> || either of them must be true
+NOT ---> ! this toggles boolean values of a perticular data
+*/
+ 
+// const userName = 'Ijeoma'
+// const age = 30
+// const logic = age === 30 || userName === 'Ijeoma'
+
+// console.log(!logic)
+
+
+// const age =30
+// console.log(20>=age)
+
+
+
+
+
+
+
+// const addition = 50%3
+
+// console.log(addition)
+
+
+
+
+// const day = 'Sunday'
+
+// const position = 1 // this is it's current position in the week
+
+// const output = 'Today is ' + day+'.' + ' It is the ' + position +'st day of the week.'
+
+// console.log(output)
+
+
+
+
+
+
+
+
+
+// const isBritish = true
+// const isNight = false
+
+// const userDetails = null
+// console.log(typeof(userDetails))
+
+
